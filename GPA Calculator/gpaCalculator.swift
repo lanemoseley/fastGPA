133f3dc4ac2fe392babead6bd32be1d9ed0da654
Lane Moseley <moseley.lane@gmail.com>
2020-02-02 02:06:23 -0700

    Constraints now working for all size phones in portrait mode

--- GPA Calculator/gpaCalculator.swift
+++ GPA Calculator/gpaCalculator.swift
@@ -8,13 +8,13 @@ import Foundation
 
 class gpaCalculator {
     // grade range
-    let grades = ["A", "---", "F", "D-", "D", "D+", "C-", "C", "C+", "B-", "B", "B+", "A-"]
+    let grades = ["A", "-", "F", "D-", "D", "D+", "C-", "C", "C+", "B-", "B", "B+", "A-"]
     
     // credit range
     let credits = [0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0]
     
     // grade->credit map
-    let gradeMap = ["---": 0.0, "A": 4.0, "A-": 3.7, "B+": 3.3, "B": 3.0, "B-": 2.7, "C+": 2.3, "C": 2.0, "C-": 1.7, "D+": 1.3, "D": 1.0, "D-": 0.7, "F": 0.0]
+    let gradeMap = ["-": 0.0, "A": 4.0, "A-": 3.7, "B+": 3.3, "B": 3.0, "B-": 2.7, "C+": 2.3, "C": 2.0, "C-": 1.7, "D+": 1.3, "D": 1.0, "D-": 0.7, "F": 0.0]
 
     private var _cumulative_gpa: Double
     private var _total_hours: Double
